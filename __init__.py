d4c7185ef9532fea353a15dc753d62773ddb345d
rayansa1234 <rayansallam5@gmail.com>
2025-12-12 10:48:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,3 +31,7 @@ def mongraphique():
     return render_template("graphique.html")
 if __name__ == "__main__":
   app.run(debug=True)
+
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
